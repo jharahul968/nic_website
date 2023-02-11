2487557e8050eddae27beea357d2bb0d31878199
Rahul Jha <jharahul968@gmail.com>
2023-02-11 09:37:46 +0545

    .

--- nic_web/nic_web/settings.py
+++ nic_web/nic_web/settings.py
@@ -142,5 +142,5 @@ MEDIA_URL = '/media/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mail.ioehub@gmail.com'
-EMAIL_HOST_PASSWORD = 'ayubjeutvhxexcam'
+EMAIL_HOST_PASSWORD = 'denialOfService123$%#'
 EMAIL_USE_TLS = True
